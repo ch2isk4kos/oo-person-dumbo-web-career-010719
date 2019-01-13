96d600b673a89e97377b9ca1f98f85d1d8701917
Chris Kakos <ckakos@gmail.com>
2019-01-13 23:30:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -20,6 +20,10 @@ class Person
     if index > 10
       happiness = 10
     end
+
+    if index < 0
+      happiness = 0
+    end
     # @happiness = 10 if index > 10
     # @happiness = 0 if index < 0
   end
