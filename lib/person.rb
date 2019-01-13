91c8de02b23b3e0c641e2b0ce71057ff5665f59d
Chris Kakos <ckakos@gmail.com>
2019-01-13 23:04:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,17 @@
 # your code goes here
 class Person
 
-  attr_reader :name
+  attr_reader :name, 
 
   def initialize(name)
     @name = name
+    @balance = 0
   end
+
+  def bank_account
+    self.balance = 25
+  end
+
+
 
 end
